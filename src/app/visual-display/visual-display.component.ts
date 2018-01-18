f64d43d880932f65af8b41067153bb3f92c4ebaa
Pablo Parra <parraman@gmail.com>
2018-01-18 13:08:57 +0100

    Changed the color palette
    Now the palete uses an 8-bits RGB scheme with 3 bits for red, 3 bits for
    green and 2 bits for blue.

--- src/app/visual-display/visual-display.component.ts
+++ src/app/visual-display/visual-display.component.ts
@@ -13,38 +13,38 @@ import { EventsLogService, SystemEvent } from '../events-log.service';
 
 const COLOR_PALETTE = [
 
-    '#000000', '#252525', '#343434', '#4E4E4E', '#686868', '#757575', '#8E8E8E', '#A4A4A4', /* 0x00..0x07 */
-    '#B8B8B8', '#C5C5C5', '#D0D0D0', '#D7D7D7', '#E1E1E1', '#EAEAEA', '#F4F4F4', '#FFFFFF', /* 0x08..0x0F */
-    '#412000', '#542800', '#763700', '#9A5000', '#C36806', '#E47B07', '#FF911A', '#FFAB1D', /* 0x10..0x17 */
-    '#FFC51F', '#FFD03B', '#FFD84C', '#FFE651', '#FFF456', '#FFF970', '#FFFF90', '#FFFFAA', /* 0x18..0x1F */
-    '#451904', '#721E11', '#9F241E', '#B33A20', '#C85120', '#E36920', '#FC8120', '#FD8C25', /* 0x20..0x27 */
-    '#FE982C', '#FFAE38', '#FFB946', '#FFBF51', '#FFC66D', '#FFD587', '#FFE498', '#FFCADE', /* 0x28..0x2F */
-    '#5D1F0C', '#7A240D', '#982C0E', '#B02F0F', '#BF3624', '#D34E2A', '#E7623E', '#F36E4A', /* 0x30..0x37 */
-    '#FD7854', '#FF8A6A', '#FF987C', '#FFA48B', '#FFB39E', '#FFC2B2', '#FFD0C3', '#FFDAD0', /* 0x38..0x3F */
-    '#4A1700', '#721F00', '#A81300', '#C8210A', '#DF2512', '#EC3B24', '#FA5236', '#FC6148', /* 0x40..0x47 */
-    '#FF705F', '#FF7E7E', '#FF8F8F', '#FF9D9E', '#FFABAD', '#FFB9BD', '#FFC7CE', '#FFCADE', /* 0x48..0x4F */
-    '#490036', '#66004B', '#80035F', '#950F74', '#AA2288', '#BA3D99', '#CA4DA9', '#D75AB6', /* 0x50..0x57 */
-    '#E467C3', '#EF72CE', '#FB7EDA', '#FF8DE1', '#FF9DE5', '#FFA5E7', '#FFAFEA', '#FFB8EC', /* 0x58..0x5F */
-    '#48036C', '#5C0488', '#650D90', '#7B23A7', '#933BBF', '#9D45C9', '#A74FD3', '#B25ADE', /* 0x60..0x67 */
-    '#BD65E9', '#C56DF1', '#CE76FA', '#D583FF', '#DA90FF', '#DE9CFF', '#E2A9FF', '#E6B6FF', /* 0x68..0x6F */
-    '#051E81', '#0626A5', '#082FCA', '#263DD4', '#444CDE', '#4F5AEC', '#5A68FF', '#6575FF', /* 0x70..0x77 */
-    '#7183FF', '#8091FF', '#90A0FF', '#97A9FF', '#9FB2FF', '#AFBEFF', '#C0CBFF', '#CDD3FF', /* 0x78..0x7F */
-    '#0B0779', '#201C8E', '#3531A3', '#4642B4', '#5753C5', '#615DCF', '#6D69DB', '#7B77E9', /* 0x80..0x87 */
-    '#8985F7', '#918DFF', '#9C98FF', '#A7A4FF', '#B2AFFF', '#BBB8FF', '#C3C1FF', '#D3D1FF', /* 0x88..0x8F */
-    '#1D295A', '#1D3876', '#1D4892', '#1D5CAC', '#1D71C6', '#3286CF', '#489BD9', '#4EA8EC', /* 0x90..0x97 */
-    '#55B6FF', '#69CAFF', '#74CBFF', '#82D3FF', '#8DDAFF', '#9FD4FF', '#B4E2FF', '#C0EBFF', /* 0x98..0x9F */
-    '#004B59', '#005D6E', '#006F84', '#00849C', '#0099BF', '#00ABCA', '#00BCDE', '#00D0F5', /* 0xA0..0xA7 */
-    '#10DCFF', '#3EE1FF', '#64E7FF', '#76EAFF', '#8BEDFF', '#9AEFFF', '#B1F3FF', '#C7F6FF', /* 0xA8..0xAF */
-    '#004800', '#005400', '#036B03', '#0E760E', '#188018', '#279227', '#36A436', '#4EB94E', /* 0xB0..0xB7 */
-    '#51CD51', '#72DA72', '#7CE47C', '#85ED85', '#99F299', '#B3F7B3', '#C3F9C3', '#CDFCCD', /* 0xB8..0xBF */
-    '#164000', '#1C5300', '#236600', '#287800', '#2E8C00', '#3A980C', '#47A519', '#51AF23', /* 0xC0..0xC7 */
-    '#5CBA2E', '#71CF43', '#85E357', '#8DEB5F', '#97F569', '#A0FE72', '#B1FF8A', '#BCFF9A', /* 0xC8..0xCF */
-    '#2C3500', '#384400', '#445200', '#495600', '#607100', '#6C7F00', '#798D0A', '#8B9F1C', /* 0xD0..0xD7 */
-    '#9EB22F', '#ABBF3C', '#B8CC49', '#C2D653', '#CDE153', '#DBEF6C', '#E8FC79', '#F2FFAB', /* 0xD8..0xDF */
-    '#463A09', '#4D3F09', '#544509', '#6C5809', '#907609', '#AB8B0A', '#C1A120', '#D0B02F', /* 0xE0..0xE7 */
-    '#DEBE3D', '#E6C645', '#EDCD4C', '#F5D862', '#FBE276', '#FCEE98', '#FDF3A9', '#FDF3BE', /* 0xE8..0xEF */
-    '#401A02', '#581F05', '#702408', '#8D3A13', '#AB511F', '#B56427', '#BF7730', '#D0853A', /* 0xF0..0xF7 */
-    '#E19344', '#EDA04E', '#F9AD58', '#FCB75C', '#FFC160', '#FFCA69', '#FFCF7E', '#FFDA96'  /* 0xF8..0xFF */
+    '#000000', '#000055', '#0000AA', '#0000FF', '#002400', '#002455', '#0024AA', '#0024FF', /* 0x00..0x07 */
+    '#004800', '#004855', '#0048AA', '#0048FF', '#006C00', '#006C55', '#006CAA', '#006CFF', /* 0x08..0x0F */
+    '#009000', '#009055', '#0090AA', '#0090FF', '#00B400', '#00B455', '#00B4AA', '#00B4FF', /* 0x10..0x17 */
+    '#00D800', '#00D855', '#00D8AA', '#00D8FF', '#00FC00', '#00FC55', '#00FCAA', '#00FCFF', /* 0x18..0x1F */
+    '#240000', '#240055', '#2400AA', '#2400FF', '#242400', '#242455', '#2424AA', '#2424FF', /* 0x20..0x27 */
+    '#244800', '#244855', '#2448AA', '#2448FF', '#246C00', '#246C55', '#246CAA', '#246CFF', /* 0x28..0x2F */
+    '#249000', '#249055', '#2490AA', '#2490FF', '#24B400', '#24B455', '#24B4AA', '#24B4FF', /* 0x30..0x37 */
+    '#24D800', '#24D855', '#24D8AA', '#24D8FF', '#24FC00', '#24FC55', '#24FCAA', '#24FCFF', /* 0x38..0x3F */
+    '#480000', '#480055', '#4800AA', '#4800FF', '#482400', '#482455', '#4824AA', '#4824FF', /* 0x40..0x47 */
+    '#484800', '#484855', '#4848AA', '#4848FF', '#486C00', '#486C55', '#486CAA', '#486CFF', /* 0x48..0x4F */
+    '#489000', '#489055', '#4890AA', '#4890FF', '#48B400', '#48B455', '#48B4AA', '#48B4FF', /* 0x50..0x57 */
+    '#48D800', '#48D855', '#48D8AA', '#48D8FF', '#48FC00', '#48FC55', '#48FCAA', '#48FCFF', /* 0x58..0x5F */
+    '#6C0000', '#6C0055', '#6C00AA', '#6C00FF', '#6C2400', '#6C2455', '#6C24AA', '#6C24FF', /* 0x60..0x67 */
+    '#6C4800', '#6C4855', '#6C48AA', '#6C48FF', '#6C6C00', '#6C6C55', '#6C6CAA', '#6C6CFF', /* 0x68..0x6F */
+    '#6C9000', '#6C9055', '#6C90AA', '#6C90FF', '#6CB400', '#6CB455', '#6CB4AA', '#6CB4FF', /* 0x70..0x77 */
+    '#6CD800', '#6CD855', '#6CD8AA', '#6CD8FF', '#6CFC00', '#6CFC55', '#6CFCAA', '#6CFCFF', /* 0x78..0x7F */
+    '#900000', '#900055', '#9000AA', '#9000FF', '#902400', '#902455', '#9024AA', '#9024FF', /* 0x80..0x87 */
+    '#904800', '#904855', '#9048AA', '#9048FF', '#906C00', '#906C55', '#906CAA', '#906CFF', /* 0x88..0x8F */
+    '#909000', '#909055', '#9090AA', '#9090FF', '#90B400', '#90B455', '#90B4AA', '#90B4FF', /* 0x90..0x97 */
+    '#90D800', '#90D855', '#90D8AA', '#90D8FF', '#90FC00', '#90FC55', '#90FCAA', '#90FCFF', /* 0x98..0x9F */
+    '#B40000', '#B40055', '#B400AA', '#B400FF', '#B42400', '#B42455', '#B424AA', '#B424FF', /* 0xA0..0xA7 */
+    '#B44800', '#B44855', '#B448AA', '#B448FF', '#B46C00', '#B46C55', '#B46CAA', '#B46CFF', /* 0xA8..0xAF */
+    '#B49000', '#B49055', '#B490AA', '#B490FF', '#B4B400', '#B4B455', '#B4B4AA', '#B4B4FF', /* 0xB0..0xB7 */
+    '#B4D800', '#B4D855', '#B4D8AA', '#B4D8FF', '#B4FC00', '#B4FC55', '#B4FCAA', '#B4FCFF', /* 0xB8..0xBF */
+    '#D80000', '#D80055', '#D800AA', '#D800FF', '#D82400', '#D82455', '#D824AA', '#D824FF', /* 0xC0..0xC7 */
+    '#D84800', '#D84855', '#D848AA', '#D848FF', '#D86C00', '#D86C55', '#D86CAA', '#D86CFF', /* 0xC8..0xCF */
+    '#D89000', '#D89055', '#D890AA', '#D890FF', '#D8B400', '#D8B455', '#D8B4AA', '#D8B4FF', /* 0xD0..0xD7 */
+    '#D8D800', '#D8D855', '#D8D8AA', '#D8D8FF', '#D8FC00', '#D8FC55', '#D8FCAA', '#D8FCFF', /* 0xD8..0xDF */
+    '#FC0000', '#FC0055', '#FC00AA', '#FC00FF', '#FC2400', '#FC2455', '#FC24AA', '#FC24FF', /* 0xE0..0xE7 */
+    '#FC4800', '#FC4855', '#FC48AA', '#FC48FF', '#FC6C00', '#FC6C55', '#FC6CAA', '#FC6CFF', /* 0xE8..0xEF */
+    '#FC9000', '#FC9055', '#FC90AA', '#FC90FF', '#FCB400', '#FCB455', '#FCB4AA', '#FCB4FF', /* 0xF0..0xF7 */
+    '#FCD800', '#FCD855', '#FCD8AA', '#FCD8FF', '#FCFC00', '#FCFC55', '#FCFCAA', '#FFFFFF', /* 0xF8..0xFF */
 
 ];
 
@@ -126,8 +126,14 @@ export class VisualDisplayComponent implements OnInit, AfterViewInit {
     private visualDisplayOperationSource = new Subject<VisualDisplayOperation>();
     private visualDisplayOperationSource$: Observable<VisualDisplayOperation>;
 
+    private initialValues = Array<number>(256);
+
     constructor(private memoryService: MemoryService,
                 private eventsLogService: EventsLogService) {
+
+        for (let i = 0; i < 256; i++) {
+            this.initialValues[i] = 0xFF;
+        }
 
         this.visualDisplayOperationSource$ = this.visualDisplayOperationSource.asObservable();
 
@@ -147,7 +153,7 @@ export class VisualDisplayComponent implements OnInit, AfterViewInit {
     ngOnInit() {
 
         this.memoryService.addMemoryRegion('VisualDisplayRegion', 0x300, 0x3FF,
-            MemoryCellAccessPermission.READ_WRITE, undefined,
+            MemoryCellAccessPermission.READ_WRITE, this.initialValues,
             (op) => this.processMemoryOperation(op));
 
     }
@@ -185,7 +191,7 @@ export class VisualDisplayComponent implements OnInit, AfterViewInit {
             pixel: offset,
             coordX: x,
             coordY: y,
-            color: value
+            color: msb
 
         };
 
@@ -198,6 +204,7 @@ export class VisualDisplayComponent implements OnInit, AfterViewInit {
             parameters.pixel = offset + 1;
             parameters.coordX = x;
             parameters.coordY = y;
+            parameters.color = lsb;
 
             this.publishVisualDisplayOperation(new VisualDisplayOperation(VisualDisplayOperationType.WRITE_PIXEL, parameters));
         }
@@ -210,7 +217,7 @@ export class VisualDisplayComponent implements OnInit, AfterViewInit {
 
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-        this.memoryService.storeBytes(0x300, 256);
+        this.memoryService.storeBytes(0x300, 256, this.initialValues);
 
     }
 
